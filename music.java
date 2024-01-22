659669ce1c9dacf26ac34d30a57a2ed39a9a011b
easierleek <61917091+easierleek@users.noreply.github.com>
2024-01-22 13:18:09 -0500

    converted to music class plus additional sound

--- music.java
+++ music.java
@@ -2,9 +2,7 @@ import javax.imageio.ImageIO;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.JOptionPane;
 import java.io.File;
 import java.util.ArrayList;
 import java.awt.image.BufferedImage;
@@ -12,76 +10,67 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
-static Clip clip;
-static Clip backgroundMusic;
-static Clip plantSound1;
-static Clip plantSound2;
-static Clip peashooter;
-static Clip wavemusic;
-static Clip endmusic;
+public class Music {
+    private static Clip clip;
+    private static Clip backgroundmusic;
+    private static Clip plantSound1;
+    private static Clip plantSound2;
+    private static Clip peashooter;
+    private static Clip wavemusic;
+    private static Clip winmusic;
+    private static Clip losemusic;
 
-
-playMusic("soundRes/Crazy_Dave.wav", true);
-
-
-playMusic("soundRes/Grasswalk.wav", true);
-
-
-playMusic("soundRes/plant.wav", false);
-playMusic("soundRes/plant2.wav", false);
-
-playMusic("soundRes/losemusic.wav", false);
-playMusic("soundRes/winmusic.wav", false);
-playMusic("soundRes/siren.wav", false);
-
-
-public static Clip playMusic(String filepath, boolean loop) {
-    try {
-        File musicPath = new File(filepath);
-        if (musicPath.exists()) {
-            AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
-            Clip newclip = AudioSystem.getClip();
-            newclip.open(audioInput);
-            if (clip != null && clip.isRunning()) {
-                clip.stop();
-                clip.close();
-            }
-
-            if (filepath.equals("soundRes/Grasswalk.wav") && backgroundMusic.isRunning()) {
-                backgroundMusic.stop();
-                backgroundMusic.close();
-            }
-
-            if (filepath.equals("soundRes/Grasswalk.wav")) {
-                backgroundMusic = newclip;
-            } else if (filepath.equals("soundRes/Crazy_Dave.wav")) {
-                backgroundMusic = newclip;
-            } 
-            else if (filepath.equals("soundRes/losemusic.wav")) {
-                losemusic = newclip;
-            } 
-            else if (filepath.equals("soundRes/winmusic.wav")) {
-                winmusic = newclip;
-            } 
-            else if(filepath.equals("soundRes/siren.wav")){
-                wavemusic = newclip;
-            }
-            else if (filepath.equals("soundRes/plant.wav")) {
-                plantSound1 = newclip;
-            } else if (filepath.equals("soundRes/plant2.wav")) {
-                plantSound2 = newclip;
-            }
-
-            // Start playing the new clip
-            if (loop) {
-                newclip.loop(Clip.LOOP_CONTINUOUSLY);
-            } else {
-                newclip.start();
-            }
-        }
-    } catch (Exception e) {
-
-        System.out.println(JOptionPane.ERROR_MESSAGE);
+    public static Clip playBackgroundMusic(String filepath, boolean loop) {
+        return playMusic(filepath, true);
     }
-    return clip;
+
+    public static Clip playWinMusic(String filepath, boolean loop) {
+        return playMusic(filepath, true);
+    }
+
+    public static Clip playLoseMusic(String filepath, boolean loop) {
+        return playMusic(filepath, false);
+    }
+
+    public static Clip playWaveMusic(String filepath, boolean loop) {
+        return playMusic(filepath, false);
+    }
+
+    public static Clip playPlantSound1(String filepath) {
+        return playMusic(filepath, false);
+    }
+
+    public static Clip playPlantSound2(String filepath) {
+        return playMusic(filepath, false);
+    }
+
+    public static Clip playPeashooter(String filepath) {
+        return playMusic(filepath, false);
+    }
+
+    public static Clip playMusic(String filepath, boolean loop) {
+        try {
+            File musicPath = new File(filepath);
+            if (musicPath.exists()) {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                Clip newclip = AudioSystem.getClip();
+                newclip.open(audioInput);
+
+                if (clip != null && clip.isRunning()) {
+                    clip.stop();
+                    clip.close();
+                }
+
+                // Start playing the new clip
+                if (loop) {
+                    newclip.loop(Clip.LOOP_CONTINUOUSLY);
+                } else {
+                    newclip.start();
+                }
+            }
+        } catch (Exception e) {
+
+            System.out.println(JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
